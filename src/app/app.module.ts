a8832284780f4cfc598a4a6886f4c8330693c583
Barış Can Yılmaz <baris@bariscanyilmaz.com>
2020-10-02 15:18:05 +0300

    add main components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { PlayerComponent } from './components/player/player.component';
+import { TopbarComponent } from './components/topbar/topbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    PlayerComponent,
+    TopbarComponent
   ],
   imports: [
     BrowserModule,
