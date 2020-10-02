0fc1f98d9c19ddd5dfad7350046c05192267017e
Barış Can Yılmaz <baris@bariscanyilmaz.com>
2020-10-02 17:58:05 +0300

    change navbar to sidebar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { PlayerComponent } from './components/player/player.component';
 import { TopbarComponent } from './components/topbar/topbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent,
+    SidebarComponent,
     PlayerComponent,
     TopbarComponent
   ],
