cc8372eca4c234e9334804d0c864d44608ba4450
Barış Can Yılmaz <baris@bariscanyilmaz.com>
2020-10-10 13:47:04 +0300

    add little genre cards to search component

--- src/app/components/genre-card/genre-card.component.ts
+++ src/app/components/genre-card/genre-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-genre-card',
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GenreCardComponent implements OnInit {
 
+  @Input('isLittle') isLittle:boolean;
   constructor() { }
 
   ngOnInit(): void {
